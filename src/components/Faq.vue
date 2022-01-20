afaf0a93d533f3075d067591e8d9c65f7636e5a8
webshum <webshumweb@gmail.com>
2022-01-20 13:09:04 +0200

    kdjf

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -11,96 +11,27 @@
 
 			<div class="link-more-dropdown">
 				<div class="accordion">
-					<div class="accordion__head">How can REX MD help if I suffer from erectile dysfunction (ED)?</div>
+					<div class="accordion__head">Is my personal information safe?</div>
 					<div class="accordion__body" style="display: none;">
-						<p>At REX MD, we believe in the power of telemedicine in making treatment quick and easy. With REX MD, it takes most patients a few minutes to complete the initial doctor visit questionnaire. Shortly after, one of our U.S. licensed physicians will review your answers and visit within a few hours.</p>
-
-						<p>Your personal doctor will need to know about your health, where your private answers about your medications, lifestyle decisions, prior surgeries, and ED will help your doctor understand the right treatment for you. They’ll also need a recent blood pressure (within last 6 months) reading, in addition to a personal ID so they know who they will be personally helping. They’ll privately review everything and determine if you’re a suitable candidate for ED treatment through telemedicine (most people are), and will come up with a personalized treatment plan.</p>
-						<p>We provide your treatment discreetly, quickly, and affordably.</p>
+						<p>We take privacy and security seriously at Rex MD. Rex MD complies with all relevant privacy and HIPAA regulations in the U.S. Our system meets strict security and encryption protocols to keep your information safe. Rex MD is LegitScript Certified, a third-party regulatory and safety reviewer.</p>
 					</div>
-					<div class="accordion__head">What is ED?</div>
+					<div class="accordion__head">What is the Lowest Price Guarantee?</div>
 					<div class="accordion__body" style="display: none;">
-						<p>ED is the inability to achieve and maintain an erection sufficient enough for sex even while sexually aroused. It is not the same as premature ejaculation, and it is not the same as low libido or the desire to have sex.</p>
-
-						<p>It’s estimated to affect as many as 40% of men by the age of 40. ED can even affect young adult men, and is sometimes referred to as impotence.</p>
-
+						<p>This is an easy one. If you find a lower price (good luck) we’ll beat it. Just contact our patient care team and let them know where you found it.</p>
 					</div>
-					<div class="accordion__head">What's the cause of ED?</div>
+					<div class="accordion__head">How is Rex MD different from other services?</div>
 					<div class="accordion__body" style="display: none;">
-						<p>The reason why our online doctor visit is so thorough is because the cause of ED can be complex. While it is most commonly caused by physical conditions, it can also be caused by psychological factors such as depression or anxiety. ED can even result as a combination of various physical and psychological factors.</p>
-
-						<p>ED can even signal the existence of more serious underlying conditions, such as heart failure. It’s important to identify this early such that it can be addressed as quickly as possible. At REX, your health comes first.</p>
+						<p>There are a number of factors that make us different, all of which we are proud of. Most importantly, we take great pride in the level of quality and care our licensed doctors put into making sure our patients are receiving the right treatment plan. Secondly, unlike our competitors, we sell the real deal at the guaranteed lowest prices. Our sildenafil tablets come in the actual 25mg, 50mg, and 100mg dosage sizes. We think that if you’re getting ready for a special time with your partner, the idea of taking 5 pills just to get and maintain an erection sounds ridiculous.</p>
 
 					</div>
-					<div class="accordion__head">Why is it important to seek help for ED?</div>
+					<div class="accordion__head">What is the process for receiving my order?</div>
 					<div class="accordion__body" style="display: none;">
-						<p>Beyond the strain that ED can place on mental well-being and relationships, on a strictly medical basis, ED can be a key warning sign of serious underlying health problems that need to be investigated further.</p>
-
-						<p>Your personal doctor at REX can help identify these warning signs, allowing you to take the right steps to protecting your health.</p>
-
+						<p>It starts with an online visit. Our doctors assess your symptoms and make a diagnosis, writing you a prescription if that’s the best course of action. Your medication is then delivered to you quickly and in discreet packaging. You can continue with a monthly subscription (the simplest way to stay up to date) and, if need be, ask your Rex MD doctor about new or ongoing issues after your initial diagnosis.</p>
 					</div>
-					<div class="accordion__head">Is it safe to use the ED service?</div>
+					<div class="accordion__head">Why shouldn't I order this product?</div>
 					<div class="accordion__body" style="display: none;">
-						<p>As long as your answer our health questionnaire fully and accurately, absolutely. Be sure to read the information we provide on the risks and benefits of treatment and treatment direction to ensure safe treatment.</p>
-
-						<p>ED medication should only be used by men who are healthy enough for sexual activity. We acknowledge that ED medication is not safe for everyone. Our licensed physicians will provide a treatment plan only after carefully determining the patient’s suitability for ED treatment.</p>
-
-					</div>
-					<div class="accordion__head">Why do you need to know my blood pressure?</div>
-					<div class="accordion__body" style="display: none;">
-						<p>Due to the way sildenafil works on blood vessels, accurate blood pressure readings play a critical role in our ability to determine if ED treatment is right for you. Your health is our number one priority, and your blood pressure measurements will enable our doctors to make the right treatment plan for you.</p>
-
-						<p>Fortunately, taking a blood pressure reading is quick and easy. Even better, your readings will tell you a general picture of your cardiovascular health! And chances are, your local pharmacy will most likely have machines that can accurately measure your blood pressure for free.</p>
-
-					</div>
-					<div class="accordion__head">How do I measure my blood pressure at home?</div>
-					<div class="accordion__body" style="display: none;">
-						<p>If you’re measuring your blood pressure at home, make sure to use a blood pressure monitor with a properly sized cuff for your upper arm. Also make sure the label on the box says ‘clinically validated’. These blood pressure monitors can be found in most pharmacies or online. Please make sure to read and follow the instructions carefully. We advise against using finger and wrist monitors due to their lower degree of accuracy.</p>
-
-						<p>Before taking your readings, make sure to rest comfortably for at least 5 minutes, and at least 30 minutes after smoking, eating, drinking caffeine, or exercising.</p>
-						<p>
-							A cuff that is the wrong size will give an inaccurate reading. You should have enough room to fit your fingertips between the cuff and your arm. The lower edge of the cuff should be located roughly an inch above your elbow. While taking the readings, rest your elbow on a table such that the cuff is around the same level vertically as your heart.
-						</p>
-						<p>For the sake of accuracy, take 2 readings in both arms, and provide us the highest number recorded.</p>
-
-					</div>
-					<div class="accordion__head">Who can use the ED service?</div>
-					<div class="accordion__body" style="display: none;">
-						<p>Our services are available to men between the ages of 30 to 73 dealing with ED. Based on the regulations of telemedicine, you must also live in one of the states where we offer services.</p>
-
-						<p>We stress that ED medication are not recreational drugs, and that you should only request a prescription if you really do suffer from ED.</p>
-
-						<p>ED medication will not address premature ejaculation or low libido. ED medication will only help you get and sustain an erection if you are sexually aroused.</p>
-						
-
-					</div>
-					<div class="accordion__head">What medications do you prescribe?</div>
-					<div class="accordion__body" style="display: none;">
-						<p>We currently prescribe either generic or branded sildenafil (Viagra made by Pfizer) in their original dosage levels: 25mg, 50mg, 100mg. These medications are only available through a doctor’s prescription and our doctors prescribe based on what they believe to be appropriate for a patient’s respective situation. These medications don’t work for women, and we don’t prescribe them to women.</p>
-
-						<p>Sildenafil works by making it easier for blood to flow through your penis. Sildenafil comes with the potential for serious side effects, particularly when taken with contraindicated medications. It is absolutely not a recreational drug.</p>
-
-						<p>In order to make sure sildenafil is right for you, our doctor visit involves a detailed set of health questions to confirm that you have ED and that it is safe for you to receive a prescription.</p>
-
-						<p>Although sildenafil is not an aphrodisiac, many men have found them to be very effective in getting and maintaining an erection while sexually aroused.</p>
-
-					</div>
-					<div class="accordion__head">Are the medications you prescribe effective?</div>
-					<div class="accordion__body" style="display: none;">
-						<p>While sildenafil has been shown to be effective for ED for the majority of men, it may not work depending on the underlying factors responsible for your ED and how your body responds to the medication.</p>
-
-						<p>In general, we advise patients to try sildenafil at a particular dose at least 3 times before making a judgment.</p>
-
-					</div>
-					<div class="accordion__head">Why can't I take these medications with nitrates or nitroglycerin?</div>
-					<div class="accordion__body" style="display: none;">
-						<p>Taking sildenafil in combination with nitrates or nitroglycerin (typically prescribed for heart conditions) can lead to a severe drop in blood pressure that could be potentially fatal.</p>
-
-					</div>
-					<div class="accordion__head">How is REX MD different from other online ED services?</div>
-					<div class="accordion__body" style="display: none;">
-						<p>There are a number of factors that make us different, all of which we are proud of. Most importantly, we take great pride in the level of quality and care our licensed doctors put into making sure our patients are receiving the right treatment plan. Secondly, unlike our competitors, we sell the real deal. Our sildenafil tablets come in the actual 25mg, 50mg, and 100mg dosage sizes. We think that if you’re getting ready for a special time with your partner, the idea of taking 5 pills just to get and maintain an erection sounds ridiculous.</p>
-					</div>
+						<p>We still can’t figure that out! Online consultations are completely free and prescriptions are written by a licensed doctor or nurse practitioner. You'll have genuine ED medications, prescribed online and delivered directly from the pharmacy at the lowest price, guaranteed.It's fast, easy, discreet and MUCH more affordable!</p>
+					</div>					
 				</div>
 			</div>
 		</div>
